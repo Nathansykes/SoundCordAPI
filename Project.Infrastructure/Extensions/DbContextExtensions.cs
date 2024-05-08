6d3cc59a74df97727f7a26d1a7953d90e7faeb76
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-05-08 16:32:25 +0100

    query filter

--- Project.Infrastructure/Extensions/DbContextExtensions.cs
+++ Project.Infrastructure/Extensions/DbContextExtensions.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Project.Domain;
+using Project.Infrastructure.Model.Entities;
 
 namespace Project.Infrastructure.Model;
 
-public partial class ApplicationDbContext : IApplicationDbContext
+public partial class ApplicationDbContext(ICurrentUserAccessor currentUserAccessor) : IApplicationDbContext
 {
+    private readonly ICurrentUserAccessor _currentUserAccessor = currentUserAccessor;
+
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.Entity<Group>()
+            .HasQueryFilter(group => group.Users.Any(x => x.Id == _currentUserAccessor.User!.Id));
     }
 }
